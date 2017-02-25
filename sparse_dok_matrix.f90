63402bddc3b388b54e4125c7e42920611972a473
Komahan Boopathy <komahan.cool@gmail.com>
2017-02-25 11:24:37 -0500

    changed variables to key

--- sparse_dok_matrix.f90
+++ sparse_dok_matrix.f90
@@ -1,6 +1,8 @@
 #include "scalar.fpp"
+
 !=====================================================================!
-! A Dictionary of keys implementation of the sparse matrix type 
+! A Dictionary of keys implementation of the sparse matrix type. This
+! is currently a constant SHAPE implementation of the matrix.
 !
 ! Author: Komahan Boopathy (komahan@gatech.edu)
 !=====================================================================!
@@ -8,23 +10,22 @@
 module sparse_matrix_dok
 
   use iso_fortran_env, only : WP => REAL64
-  use sparse_matrix_interface, only: sparse_matrix
+  use sparse_matrix_interface, only : sparse_matrix
   
   implicit none
-
+  
   private
- 
+  
   public :: dok_matrix
   
   ! Dictionary of keys sparse matrix
   type, extends(sparse_matrix) :: dok_matrix
      
-     type(integer), allocatable :: cols(:)
-     type(integer), allocatable :: rows(:)
+     type(integer), allocatable :: keys(:,:)
      type(scalar) , allocatable :: vals(:)
-
+     
    contains
-
+     
      ! Implemented procedures
      procedure :: add_entry => add_sparse_entry
      procedure :: get_entry => get_sparse_entry
@@ -34,7 +35,7 @@ module sparse_matrix_dok
 
   end type dok_matrix
 
-  ! Interfaces
+  ! Interfaces to construct this matrix
   interface dok_matrix
      procedure construct_dok_from_empty
      procedure construct_dok_from_data
@@ -55,10 +56,12 @@ module sparse_matrix_dok
       call this % set_num_nonzeros(nnz)
 
       ! Allocate space
+      allocate(this % keys(2,this % get_num_nonzeros()))
       allocate(this % vals(this % get_num_nonzeros()))
 
       ! Zero the entries
       this % vals = 0.0_WP
+      this % keys = -1
 
     end function construct_dok_from_empty
 
@@ -72,7 +75,6 @@ module sparse_matrix_dok
       type(integer), intent(in) :: rows(:)
       type(integer), intent(in) :: cols(:)
       type(scalar), intent(in)  :: vals(:)
-
       
 !!$    ! Bounds and values are copied
 !!$    allocate(this % vals, source = mat) ! mold = mat copies just the bounds
@@ -80,11 +82,11 @@ module sparse_matrix_dok
 !!$    ! Set matrix dimensions before return
 !!$    call this % set_row_size( this % get_row_size() )
 !!$    call this % set_col_size( this % get_col_size() )
-
+      
       associate( size_vals => size(vals), &
            size_rows => size(rows), &
            size_cols => size(cols) )
-       
+        
         if ( size_vals .eq. size_cols .and. &
              size_vals .eq. size_rows ) then
            
@@ -96,14 +98,15 @@ module sparse_matrix_dok
       end associate
       
       ! Allocate space
+      allocate(this % keys(2, this % get_num_nonzeros()))
       allocate(this % vals(this % get_num_nonzeros()))
-      allocate(this % rows(this % get_num_nonzeros()))
-      allocate(this % cols(this % get_num_nonzeros()))
 
       ! Copy the data and nonzero indices
+      this % keys(1,:) = rows
+      this % keys(2,:) = cols
       this % vals = vals
-      this % rows = rows
-      this % cols = cols
+
+      ! Sort the keys, may be?
       
     end function construct_dok_from_data
     
@@ -119,13 +122,9 @@ module sparse_matrix_dok
       if (allocated(this % vals)) then
          deallocate(this % vals)
       end if
-      
-      if (allocated(this % rows)) then
-         deallocate(this % rows)
-      end if
-
-      if (allocated(this % cols)) then
-         deallocate(this % cols)
+     
+      if (allocated(this % keys)) then
+         deallocate(this % keys)
       end if
 
       print *, "Destructing sparse matrix!"
@@ -156,9 +155,7 @@ module sparse_matrix_dok
       class(dok_matrix), intent(in) :: this
       type(integer), intent(in) :: row
       type(integer), intent(in) :: col
-
-      ! val = 
-      
+            
     end function get_sparse_entry
     
   end module sparse_matrix_dok
